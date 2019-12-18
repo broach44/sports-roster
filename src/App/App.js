8cdecb258bb8fde97cdd719d9836e5a76495ac90
Crystal Broach <crysland@gmail.com>
2019-12-17 21:34:37 -0600

    auth complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import firebase from 'firebase/app';
+
+import firebaseConnection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
 
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <button className='btn btn-danger'>HELP ME</button>
-    </div>
-  );
+firebaseConnection.firebaseApp();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Auth />
+      </div>
+    );
+  }
 }
 
 export default App;
