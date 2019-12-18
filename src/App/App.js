c3713c316672e8750c5c0dd4d3e63c5557aa5417
Crystal Broach <crysland@gmail.com>
2019-12-17 20:57:40 -0600

    seed data complete and uploaded to firebase

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className='btn btn-danger'>HELP ME</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='btn btn-danger'>HELP ME</button>
     </div>
   );
 }
